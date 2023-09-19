c88d3fda515af738eec6da8c5ae0fb7fd4a49925
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-09-18 18:19:16 -0700

    added content to README.md, updated JavaScript files, safe revert point

---
+++ README.md
@@ -0,0 +1,85 @@
+# SVG Logo Maker
+  [![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+## Description
+
+This is a Node.js command-line application that takes in user input to generate a logo and saves it as an SVG file. This allows the user to easily make a simple logo by answering 4 questions in their terminal or Git Bash. Please see User Story and Acceptance Criteria below:
+
+User Story: 
+
+AS a freelance web developer
+I WANT to generate a simple logo for my projects
+SO THAT I don't have to pay a graphic designer
+
+Acceptance Criteria:
+
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named "logo.svg"
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the "logo.svg" file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+
+
+## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+
+## Installation
+
+Please download Git BASH at https://gitforwindows.org/ if you are a Windows user. If you are a Mac user, please pull up the terminal on your Mac by pressing Command Spacebar and searching for terminal. 
+
+Please also install the following programs: 
+
+    * node.js at https://nodejs.dev/en/download/
+    * Inquirer.js Version 8.2.4 at https://www.npmjs.com/package/inquirer
+    * VScode at https://code.visualstudio.com/download
+    * Jest Version 29.7.0 at https://jestjs.io/
+
+
+## Usage
+
+In order to use this application, open up your terminal (on Mac) or Git Bash (on Windows). CD into the folder that contains the SVG Logo Maker code and type ‘node index.js’ into your terminal (without the single quotations). You will be prompted with questions regarding the logo you would like to create. Once you answer each question, a logo.svg file will be created and added to the folder that contains the SVG Logo Maker code. If you already have a logo.svg file, it will be overwritten with the new logo. 
+
+## Demo
+
+Please see a demo of my application [here](https://drive.google.com/file/d/1ymB5xPjwDnNUlaStpg9w4nJ8Qoi1KO-5/view?usp=sharing).
+
+## Credits
+
+SVG has been developed by the World Wide Web Consortium (W3C) since 1999. See below for link to the W3C website: https://www.w3.org/TR/SVG2/
+
+Badge for MIT License was made with [Shields.io](http://shields.io/) and taken from GitHub user: lukas-h. See below for the link to the license badge collection: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+
+
+## License
+
+MIT License, please refer to the LICENSE in the repo.
+
+## Contributing
+
+N/A
+
+## Tests
+
+This application uses Jest to run unit tests. If you would like to run the unit tests found in shapes.test.js, type in 'npm run test' in your terminal (without the single quotations).
+
+## Questions
+
+Please refer to my profile for additional projects: https://github.com/Vannasaur
+
+If you have any questions please email me at: vannaluciano@gmail.com
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const questions = [
         name: 'text'
     }, {
         type: 'input',
-        message: 'Please enter the text color you would like, either by color keyword or hexadecimal color code.',
+        message: 'Please enter the text color you would like, either by color keyword or hexadecimal color code. If using a hexadecimal color code, you must include the #.',
         name: 'textColor'
     }, {
         type: 'list',
@@ -24,7 +24,7 @@ const questions = [
         ]
     }, {
         type: 'input',
-        message: 'Please enter the shape color you would like, either by color keyword or hexadecimal color code.',
+        message: 'Please enter the shape color you would like, either by color keyword or hexadecimal color code. If using a hexadecimal color code, you must include the #.',
         name: 'shapeColor'
     }
 ];
@@ -56,28 +56,30 @@ const writeToFile = (userInput) => {
 // Function to validate color keywords:
 // Color list taken from W3Schools at: https://www.w3schools.com/colors/colors_names.asp
 const isValidColorKeyword = (color) => {
-    const ValidColorKeywords = ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure',
-    'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue', 'BlueViolet',
-    'Brown', 'BurlyWood',
-    'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue',
-    'Cornsilk', 'Crimson', 'Cyan',
-    'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGrey', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DimGrey', 'DodgerBlue',
-    'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia',
-    'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod', 'Gray', 'Grey', 'Green', 'GreenYellow',
-    'HoneyDew', 'HotPink',
-    'IndianRed', 'Indigo', 'Ivory',
-    'Khaki',
-    'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey', 'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen',
-    'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin',
-    'NavajoWhite', 'Navy',
-    'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid',
-    'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple',
-    'RebeccaPurple', 'Red', 'RosyBrown', 'RoyalBlue',
-    'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue',
-    'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise',
-    'Violet',
-    'Wheat', 'White', 'WhiteSmoke',
-    'Yellow', 'YellowGreen'];
+    const ValidColorKeywords = [
+        'aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure',
+        'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet',
+        'brown', 'burlywood',
+        'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue',
+        'cornsilk', 'crimson', 'cyan',
+        'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgrey', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue',
+        'firebrick', 'floralwhite', 'forestgreen', 'fuchsia',
+        'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'grey', 'green', 'greenyellow',
+        'honeydew', 'hotpink',
+        'indianred', 'indigo', 'ivory',
+        'khaki',
+        'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgrey', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen',
+        'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin',
+        'navajowhite', 'navy',
+        'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid',
+        'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple',
+        'rebeccapurple', 'red', 'rosybrown', 'royalblue',
+        'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue',
+        'tan', 'teal', 'thistle', 'tomato', 'turquoise',
+        'violet',
+        'wheat', 'white', 'whitesmoke',
+        'yellow', 'yellowgreen'
+    ];
 
     return ValidColorKeywords.includes(color.toLowerCase());
 };
@@ -85,7 +87,7 @@ const isValidColorKeyword = (color) => {
 // Function to validate hexadecimal color codes:
 const isValidHexCode = (color) => {
 // regular expression to validate a 3-digit or 6-digit hexadecimal color code with an optional # at the beginning
-    const hexCodeRegex = /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
+    const hexCodeRegex = /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})|[A-Fa-f0-9]{3}|[A-Fa-f0-9]{6}$/;
 
     return hexCodeRegex.test(color);
 };
@@ -106,7 +108,7 @@ const init = () => {
             init(); // prompt the user to enter input again
             return;
         // validation check for shape color input
-        } else if (!isValidColorKeyword(userInput.shapeColor) && !isValidHexCode(userInput.shapeColor)) {
+        } else if (!isValidColorKeyword(userInput.textColor) && !isValidHexCode(userInput.shapeColor)) {
             console.error('Invalid shape color input. Please enter a valid color keyword or hexadecimal color code.');
             init(); // prompt the user to enter input again
             return;
